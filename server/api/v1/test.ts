e004802e69d83d839cab4c94fe8bdfa7efaa534a
Sergio Carvalho <sergiopcn@gmail.com>
2025-06-24 11:49:26 -0300

    refactor(test): remove unused test endpoint
    - Delete the test endpoint for the Sticker.ly API Wrapper.

--- server/api/v1/test.ts
+++
@@ -1,6 +0,0 @@
-export default defineCachedEventHandler(async (event) => {
-  return useFormatter(true, 'This is a test endpoint for the Sticker.ly API Wrapper.', {
-    method: event.method,
-    path: event.path
-  })
-}, { swr: true, maxAge: 5, staleMaxAge: 60 * 60 })
