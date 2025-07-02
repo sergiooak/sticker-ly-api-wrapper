6fb5d066799442f735d16d6f3ee513b33af409c2
Sergio Carvalho <sergiopcn@gmail.com>
2025-07-02 16:39:22 -0300

    refactor: replace landing markdown with component

--- content.config.ts
+++ content.config.ts
@@ -2,15 +2,10 @@ import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    landing: defineCollection({
-      type: 'page',
-      source: 'index.md'
-    }),
     docs: defineCollection({
       type: 'page',
       source: {
-        include: '**',
-        exclude: ['index.md']
+        include: '**'
       },
       schema: z.object({
         links: z.array(z.object({
